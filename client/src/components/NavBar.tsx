1b8e06f6c4161ef3560d4827c621000bad87c154
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-20 19:04:16 +1200

    feat: Responsive navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,48 +1,114 @@
 import sal from "../assets/sal.png";
 import { NavLink } from "react-router";
+import { useState } from "react";
+import NavButton from "./NavButton";
+import '../utils/NavBar.css';
+
 function NavBar() {
-    return (
-        <div className="w-screen">
-        <div className="bg-cream m-4 p-2 text-black rounded-4xl flex flex-row items-center justify-between align-center">
-            <div className="w-1/6 flex align-content-center">
-                <div>
-                <img src={sal} className="w-2/3 m-1 ml-4"/>
-                </div>
-                <NavLink to="/" className="text-red font-title text-5xl self-center h-fit transition-colors duration-300 ease-in-out hover:text-brown">UADS</NavLink>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="sticky top-0 z-50 w-screen">
+      {/* Navbar container */}
+      <div
+        className={`navbar-container bg-cream my-3 mx-8 md:my-4 md:mx-4 p-2 text-black rounded-4xl md:rounded-full border-red border-2 md:border-4 ${isOpen ? 'open' : ''}`}
+
+      >
+        <div className="flex flex-row items-center justify-between h-12 sm:h-16 md:h-20">
+          {/* Sal + Logo + Menu button */}
+          <div className="sm:w-screen md:w-1/4 lg:w-1/3 xl:w-1/5 2xl:w-1/6 flex flex-row pl-2 justify-between md:justify-start items-center">
+            <img
+              src={sal}
+              className={`w-1/8 sm:w-1/10 md:w-1/3 lg:w-1/4 xl:w-1/4 2xl:w-2/3 m-1 xl:m-4 h-fit self-center transition-transform duration-500 ${
+              isOpen ? "rotate-360" : ""}`} 
+            />
+            <NavLink
+              to="/"
+              className="md:flex text-red font-title text-4xl sm:text-5xl md:text-4xl lg:text-5xl self-center h-fit transition-colors duration-300 ease-in-out hover:text-brown "
+            >
+              UADS
+            </NavLink>
+            <div className="md:hidden self-center">
+            <p
+                onClick={() => setIsOpen(!isOpen)}
+                className="relative cursor-pointer sm:px-5 py-3 rounded-full font-body font-bold w-fit min-w-[90px] text-center transition-colors duration-300 ease-in-out bg-red text-cream hover:bg-brown"
+                >
+                <span className="invisible">Close</span>
+                <span className="absolute inset-0 flex items-center justify-center">
+                    {isOpen ? 'Close' : 'Menu'}
+                </span>
+            </p>
             </div>
-            
-            <div className="flex flex-row pr-4 w-fit">
-                <div className="px-8">
-                    <NavLink to="/sponsors" className="font-body font-bold sm:text-xl md:text-lg lg:text-xl xl:text-2xl text-red hover:text-brown transition-colors duration-300 ease-in-out">Sponsors</NavLink>   
-                </div>
-                <div className="px-8">
-                    <NavLink to="/events" className="font-body font-bold sm:text-xl md:text-lg lg:text-xl xl:text-2xl text-red hover:text-brown transition-colors duration-300 ease-in-out">Events</NavLink>   
-                </div>
-                <div className="px-8">
-                    <NavLink to="/about" className="font-body font-bold sm:text-xl md:text-lg lg:text-xl xl:text-2xl text-red hover:text-brown transition-colors duration-300 ease-in-out">About</NavLink>   
-                </div>
-                <div className="pl-8">
-                <Button linkto="/join" buttontext="Join Us"></Button>
-                </div>
+          </div>
+
+          {/* Widescreen nav */}
+          <div className="hidden md:flex flex-row pr-4 w-fit self-center items-center">
+            <div className="md:px-5 lg:px-8">
+              <NavLink
+                to="/sponsors"
+                className="font-body font-bold sm:text-xl md:text-md lg:text-xl xl:text-2xl text-red hover:text-brown transition-colors duration-300 ease-in-out"
+              >
+                Sponsors
+              </NavLink>
             </div>
-            
+            <div className="md:px-5 lg:px-8">
+              <NavLink
+                to="/events"
+                className="font-body font-bold sm:text-xl md:text-md lg:text-xl xl:text-2xl text-red hover:text-brown transition-colors duration-300 ease-in-out"
+              >
+                Events
+              </NavLink>
+            </div>
+            <div className="md:px-5 lg:px-8">
+              <NavLink
+                to="/about"
+                className="font-body font-bold sm:text-xl md:text-md lg:text-xl xl:text-2xl text-red hover:text-brown transition-colors duration-300 ease-in-out"
+              >
+                About
+              </NavLink>
+            </div>
+            <NavButton linkto="/join" buttontext="Join Us" />
           </div>
         </div>
-      );
 
-}
-
-function Button({ linkto, buttontext }) {
-    return (
-        <div>
-            <NavLink
-                to={linkto}
-                className= "px-5 sm:px-10 md:px-6 lg:px-10 py-3 rounded-full font-body font-bold sm:text-xl md:text-lg lg:text-xl xl:text-2xl w-fit transition-colors duration-300 ease-in-out bg-red text-cream hover:bg-brown"
+        {/* Mobile dropdown nav */}
+        <div
+            className={`md:hidden flex flex-col items-center gap-4 transition-all duration-500 ease-in-out overflow-hidden ${
+            isOpen ? 'opacity-100 max-h-96 pt-4' : 'opacity-0 max-h-0 pt-0'
+            }`}
             >
-                {buttontext}
+          <NavLink
+            to="/sponsors"
+            className="block text-red font-body font-semibold hover:text-brown transition"
+            onClick={() => setIsOpen(false)}
+          >
+            Sponsors
+          </NavLink>
+          <NavLink
+            to="/events"
+            className="block text-red font-body font-semibold hover:text-brown transition"
+            onClick={() => setIsOpen(false)}
+          >
+            Events
+          </NavLink>
+          <NavLink
+            to="/about"
+            className="block text-red font-body font-semibold hover:text-brown transition"
+            onClick={() => setIsOpen(false)}
+          >
+            About
+          </NavLink>
+          <NavLink 
+            to="/join" 
+            className= "w-1/2 text-center sm:px-5 py-1 rounded-full font-body font-semibold transition-colors duration-300 ease-in-out bg-red text-cream hover:bg-brown" 
+            onClick={() => setIsOpen(false)}
+            >
+            Join Us
             </NavLink>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
---
+++ client/src/utils/FrontendTypes.ts
@@ -0,0 +1,5 @@
+export interface NavButtonProps {
+    linkto: string;
+    buttontext: string;
+  }
+
