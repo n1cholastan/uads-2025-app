5f06f554f9504ef6eec76b21f3b8d81ca1567e7f
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-22 03:38:58 +1200

    style: star sway

--- client/src/components/StarSeparator.tsx
+++ client/src/components/StarSeparator.tsx
@@ -1,7 +1,7 @@
 import star from "../assets/star.svg";
 function StarSeparator() {
     return (
-        <div className="my-16 flex justify-center items-center">
+        <div className="my-16 flex justify-center items-center animate-sway">
             <img src={star} className="w-16"></img>
         </div>
     )
