39f7b49f451c178e5d58625cb09a5d4f8dbf0f54
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-23 22:09:14 +1200

    style: controller formatting

--- server/controllers/EventController.ts
+++ server/controllers/EventController.ts
@@ -1,61 +1,68 @@
-import { Request, Response } from "express";
-import { Client } from "@notionhq/client";
-import { EventStructured } from "../utils/BackendTypes";
-import { config } from "dotenv";
+import { Request, Response } from 'express';
+import { Client } from '@notionhq/client';
+import { EventStructured } from '../utils/BackendTypes';
+import { config } from 'dotenv';
 config();
 
 const notionSecret = process.env.NOTION_TOKEN;
 const eventsID = process.env.NOTION_EVENTS_DB_ID;
 
 const notion = new Client({
-    auth: notionSecret,
+  auth: notionSecret,
 });
 
 export const getEvents = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const response = await notion.databases.query({
-            database_id: eventsID as string,
-        });
+  try {
+    const response = await notion.databases.query({
+      database_id: eventsID as string,
+    });
 
-        const events: EventStructured[] = response.results.map((page: any) => {
-            const title = page.properties.Title.title[0]?.text?.content || "Title TBD";
-            const location = page.properties.Location.rich_text[0]?.text?.content || "Location to be confirmed";
-            const description = page.properties.Description.rich_text[0]?.text?.content || "Coming Soon";
-            const imageSrc = page.properties.Image.files[0]?.file?.url || "";
-            const eventStartDateTime = page.properties.Date.date?.start || "";
-            const eventEndDateTime = page.properties.Date.date?.end || "";
-            const form = page.properties.Form.url || "";
-            const week = page.properties.Week.rich_text[0]?.text?.content || "Date TBD";
-            return {
-                id: page.id,
-                title,
-                location,
-                description,
-                imageSrc,
-                eventStartDateTime,
-                eventEndDateTime,
-                form,
-                week,
-            };
+    const events: EventStructured[] = response.results.map((page: any) => {
+      const title = page.properties.Title.title[0]?.text?.content || 'Title TBD';
+      const location =
+        page.properties.Location.rich_text[0]?.text?.content || 'Location to be confirmed';
+      const description = page.properties.Description.rich_text[0]?.text?.content || 'Coming Soon';
+      const imageSrc = page.properties.Image.files[0]?.file?.url || '';
+      const eventStartDateTime = page.properties.Date.date?.start || '';
+      const eventEndDateTime = page.properties.Date.date?.end || '';
+      const form = page.properties.Form.url || '';
+      const week = page.properties.Week.rich_text[0]?.text?.content || 'Date TBD';
+      return {
+        id: page.id,
+        title,
+        location,
+        description,
+        imageSrc,
+        eventStartDateTime,
+        eventEndDateTime,
+        form,
+        week,
+      };
     });
 
     const now = new Date();
     const upcomingEvents = events
-    .filter(event => new Date(event.eventStartDateTime) >= now)
-    .sort((a, b) => new Date(a.eventStartDateTime).getTime() - new Date(b.eventStartDateTime).getTime());
+      .filter((event) => new Date(event.eventStartDateTime) >= now)
+      .sort(
+        (a, b) =>
+          new Date(a.eventStartDateTime).getTime() - new Date(b.eventStartDateTime).getTime()
+      );
 
     const pastEvents = events
-    .filter(event => new Date(event.eventStartDateTime) < now)
-    .sort((a, b) => new Date(b.eventStartDateTime).getTime() - new Date(a.eventStartDateTime).getTime());
+      .filter((event) => new Date(event.eventStartDateTime) < now)
+      .sort(
+        (a, b) =>
+          new Date(b.eventStartDateTime).getTime() - new Date(a.eventStartDateTime).getTime()
+      );
 
     const sortedEvents = [...upcomingEvents, ...pastEvents];
 
     res.status(200).json(sortedEvents);
-    } catch (error) {
-    console.error("Error fetching events:", error);
-    res.status(500).json({ message: "Failed to fetch events"})
-    }
-}
+  } catch (error) {
+    console.error('Error fetching events:', error);
+    res.status(500).json({ message: 'Failed to fetch events' });
+  }
+};
 
 export const getEventById = async (req: Request, res: Response) => {
   const { id } = req.params;
@@ -65,36 +72,37 @@ export const getEventById = async (req: Request, res: Response) => {
     });
 
     const events: EventStructured[] = response.results.map((page: any) => {
-      const title = page.properties.Title.title[0]?.text?.content || "Title TBD";
-      const location = page.properties.Location.rich_text[0]?.text?.content || "Location to be confirmed";
-      const description = page.properties.Description.rich_text[0]?.text?.content || "Coming Soon";
-      const imageSrc = page.properties.Image.files[0]?.file?.url || "";
-      const eventStartDateTime = page.properties.Date.date?.start || "";
-      const eventEndDateTime = page.properties.Date.date?.end || "";
-      const form = page.properties.Form.url || "";
-      const week = page.properties.Week.rich_text[0]?.text?.content || "Date TBD";
+      const title = page.properties.Title.title[0]?.text?.content || 'Title TBD';
+      const location =
+        page.properties.Location.rich_text[0]?.text?.content || 'Location to be confirmed';
+      const description = page.properties.Description.rich_text[0]?.text?.content || 'Coming Soon';
+      const imageSrc = page.properties.Image.files[0]?.file?.url || '';
+      const eventStartDateTime = page.properties.Date.date?.start || '';
+      const eventEndDateTime = page.properties.Date.date?.end || '';
+      const form = page.properties.Form.url || '';
+      const week = page.properties.Week.rich_text[0]?.text?.content || 'Date TBD';
       return {
-          id: page.id,
-          title,
-          location,
-          description,
-          imageSrc,
-          eventStartDateTime,
-          eventEndDateTime,
-          form,
-          week,
+        id: page.id,
+        title,
+        location,
+        description,
+        imageSrc,
+        eventStartDateTime,
+        eventEndDateTime,
+        form,
+        week,
       };
-});
+    });
 
     const event = events.find((e) => e.id === id);
 
     if (!event) {
-      return res.status(404).json({ error: "Event not found" });
+      return res.status(404).json({ error: 'Event not found' });
     }
 
     res.json(event);
   } catch (err) {
-    console.error("Error fetching event:", err);
-    res.status(500).json({ error: "Failed to fetch event" });
+    console.error('Error fetching event:', err);
+    res.status(500).json({ error: 'Failed to fetch event' });
   }
 };
--- server/controllers/ExecController.ts
+++ server/controllers/ExecController.ts
@@ -1,44 +1,44 @@
-import { Request, Response } from "express";
-import { Client } from "@notionhq/client";
-import { ExecStructured } from "../utils/BackendTypes";
-import { config } from "dotenv";
+import { Request, Response } from 'express';
+import { Client } from '@notionhq/client';
+import { ExecStructured } from '../utils/BackendTypes';
+import { config } from 'dotenv';
 config();
 
 const notionSecret = process.env.NOTION_TOKEN;
 const execID = process.env.NOTION_EXECS_DB_ID;
 
 const notion = new Client({
-    auth: notionSecret,
+  auth: notionSecret,
 });
 
-export const getExecs = async(req: Request, res: Response): Promise<void> => {
-    try {
-        const response = await notion.databases.query({
-            database_id: execID as string,
-            sorts: [
-                {
-                  property: "Order",
-                  direction: "ascending",
-                },
-              ]
-        })
+export const getExecs = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const response = await notion.databases.query({
+      database_id: execID as string,
+      sorts: [
+        {
+          property: 'Order',
+          direction: 'ascending',
+        },
+      ],
+    });
 
-        const execs: ExecStructured[] = response.results.map((page: any) => {
-            const name = page.properties.Title.title[0]?.text?.content || "No Name";
-            const imageSrc = page.properties.Image.files[0]?.file?.url || "";
-            const team = page.properties.Team.select?.name || "General Executive";
+    const execs: ExecStructured[] = response.results.map((page: any) => {
+      const name = page.properties.Title.title[0]?.text?.content || 'No Name';
+      const imageSrc = page.properties.Image.files[0]?.file?.url || '';
+      const team = page.properties.Team.select?.name || 'General Executive';
 
-            return {
-                id: page.id,
-                name,
-                imageSrc,
-                team,
-            };
-        });
+      return {
+        id: page.id,
+        name,
+        imageSrc,
+        team,
+      };
+    });
 
-        res.status(200).json(execs);
-    } catch (error) {
-        console.error("Error fetching execs:", error);
-        res.status(500).json({ message: "Failed to fetch execs "})
-    }
-}
+    res.status(200).json(execs);
+  } catch (error) {
+    console.error('Error fetching execs:', error);
+    res.status(500).json({ message: 'Failed to fetch execs ' });
+  }
+};
