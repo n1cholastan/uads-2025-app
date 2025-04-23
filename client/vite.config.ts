3b92057b8f2d189076378fedc2aeb50bfc101605
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-23 19:20:11 +1200

    feat: setting up dev proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
-  ],
-})
+    tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // or whatever your backend port is
+        changeOrigin: true,
+      },
+    },
+  },
+});
