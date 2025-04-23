53f82adff7fcb2660b0a059710f382d5f9ce5518
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-23 22:11:59 +1200

    refactor: removing useless types

--- server/utils/BackendTypes.ts
+++ server/utils/BackendTypes.ts
@@ -1,28 +1,28 @@
 export type EventStructured = {
-    id: string;
-    eventStartDateTime: string; // in ISO format
-    eventEndDateTime: string;
-    title: string;
-    location: string;
-    description: string;
-    imageSrc: string;
-    form: string;
-    week: string;
-  }
+  id: string;
+  eventStartDateTime: string; // in ISO format
+  eventEndDateTime: string; // in ISO format
+  title: string;
+  location: string;
+  description: string;
+  imageSrc: string;
+  form: string;
+  week: string;
+};
 
 export type SponsorStructured = {
-    id: string;
-    name: string;
-    location: string;
-    isDeal: boolean;
-    deal: string;
-    imageSrc: string;
-    link: string;
-  };
+  id: string;
+  name: string;
+  location: string;
+  isDeal: boolean;
+  deal: string;
+  imageSrc: string;
+  link: string;
+};
 
 export type ExecStructured = {
-    id: string;
-    name: string;
-    imageSrc: string;
-    team: string;
-  }  
+  id: string;
+  name: string;
+  imageSrc: string;
+  team: string;
+};
