460d0fdcf722a6eed58d7a71189773e830a4b381
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-23 22:19:08 +1200

    refactor: general reformatting

--- client/src/components/StarSeparator.tsx
+++ client/src/components/StarSeparator.tsx
@@ -1,10 +1,10 @@
-import star from "../assets/star.svg";
+import star from '../assets/star.svg';
 function StarSeparator() {
-    return (
-        <div className="my-16 flex justify-center items-center animate-sway">
-            <img src={star} className="w-16"></img>
-        </div>
-    )
+  return (
+    <div className="my-16 flex justify-center items-center animate-sway">
+      <img src={star} className="w-16"></img>
+    </div>
+  );
 }
 
 export default StarSeparator;
