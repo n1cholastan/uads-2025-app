9be7d0aa471fc7264a7b33a8bb58b0d977506ff9
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-23 22:15:00 +1200

    feat: scroll reset

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Events from './pages/Events';
@@ -7,26 +6,27 @@ import AboutUs from './pages/AboutUs';
 import Sponsors from './pages/Sponsors';
 import SpecificEventPage from './pages/SpecificEventPage';
 import NavBar from './components/NavBar';
-import './App.css'
+import ScrollToTop from './components/ScrollToTop';
+import './App.css';
 
 function App() {
-
   return (
-      <Router>
-        <div className='flex flex-col bg-cream'>
-          <NavBar />
-          <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/*" element={<Home />} />
-            <Route path="/events/:eventId" element={<SpecificEventPage />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/sponsors" element={<Sponsors />} />
-            <Route path="/about" element={<AboutUs />} />
-          </Routes>
-          <Footer/>
-        </div>
-      </Router> 
-  )
+    <Router>
+      <ScrollToTop />
+      <div className="flex flex-col bg-cream">
+        <NavBar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/*" element={<Home />} />
+          <Route path="/events/:eventId" element={<SpecificEventPage />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
