9f17fc287d8af4dd24bc4cebfd7632c19d851498
Nicholas Tan <163990283+n1cholastan@users.noreply.github.com>
2025-04-23 22:17:41 +1200

    refactor: NavButton formatting

--- client/src/components/NavButton.tsx
+++ client/src/components/NavButton.tsx
@@ -1,16 +1,16 @@
-import { NavLink } from "react-router";
-import { NavButtonProps } from "../utils/FrontendTypes";
-function NavButton({ linkto, buttontext} : NavButtonProps) {
-    return (
-        <div>
-            <NavLink
-                to={linkto}
-                className= "px-5 sm:px-10 md:px-5 lg:px-10 py-3 rounded-full font-body font-bold sm:text-xl md:text-lg lg:text-xl xl:text-2xl w-fit transition-colors duration-300 ease-in-out bg-red text-cream hover:bg-brown"
-            >
-                {buttontext}
-            </NavLink>
-        </div>
-    );
+import { NavLink } from 'react-router';
+import { NavButtonProps } from '../utils/FrontendTypes';
+function NavButton({ linkto, buttontext }: NavButtonProps) {
+  return (
+    <div>
+      <NavLink
+        to={linkto}
+        className="px-5 sm:px-10 md:px-5 lg:px-10 py-3 rounded-full font-body font-bold sm:text-xl md:text-lg lg:text-xl xl:text-2xl w-fit transition-colors duration-300 ease-in-out bg-red text-cream hover:bg-brown"
+      >
+        {buttontext}
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavButton;
